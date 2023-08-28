cdb917c29f45f8e1a79fc571f754c20c185b4676
merkushynArtem <merkushynwrite@gmail.com>
2023-08-28 21:21:55 +0300

    added middleware

--- server/index.js
+++ server/index.js
@@ -1,10 +1,19 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 const app = express();
 
 //constants
 const PORT = 3002;
+
+//middleware
+app.use(cors()); //для того щоб можно було відправляти з різних ip, запроси до нашого серверу
+app.use(express.json()); //для  того щоб express розумів що дані з фронту будуть приходити у форматі json
+
+app.get('/', (req, res) => {
+   return res.json({ massage: 'All is fine' });
+});
 
 async function start() {
    try {
