a496242b7787203794b9ed6cc66e75b389d39cbb
merkushynArtem <merkushynwrite@gmail.com>
2023-08-28 19:57:41 +0300

    connected to mongodb

--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
 import express from "express";
+import mongoose from "mongoose";
 
 const app = express();
 
-app.listen(5000, () => {
-   console.log('server started');
-});
+//constants
+const PORT = 3002;
+
+async function start() {
+   try {
+      await mongoose.connect(
+         'mongodb+srv://postgram:postgram13@cluster0.3cpg3qu.mongodb.net/postgram?retryWrites=true&w=majority'
+      );
+
+      app.listen(PORT, () => console.log(`server started on port: ${PORT}`));
+   } catch(error) {
+      console.log(error);
+   }
+}
+start();
